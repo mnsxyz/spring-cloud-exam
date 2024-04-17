183c26ebefea786f5601cd30f353a2b86eacac35
mnsxyz <ms7eo69@gmail.com>
2024-04-17 14:45:53 +0900

    f

---
+++ src/main/kotlin/com/example/springcloudexam/SpringCloudExamApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springcloudexam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudExamApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCloudExamApplication>(*args)
+}
