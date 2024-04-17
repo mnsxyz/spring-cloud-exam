9a8579ef92a9bf3a9e7b37afa455831a872d388b
mnsxyz <ms7eo69@gmail.com>
2024-04-17 16:12:19 +0900

    f

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ subprojects {
 
     dependencies {
         implementation("org.jetbrains.kotlin:kotlin-reflect")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
     }
 
     dependencyManagement {
